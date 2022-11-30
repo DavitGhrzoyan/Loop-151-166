ab756d2ab8f9201c2bbccd362b2cb29f325167e5
Davit <ghrzoyandavit@gmail.com>
2022-11-30 12:25:13 +0400

    Add project files.

---
+++ Loop 151-166/Program.cs
@@ -0,0 +1,230 @@
+﻿using System;
+
+namespace Loop_Exercises_151_166
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Xndir 151
+            //int number = 70;
+            //int sum = 0;
+            //for (int i = 1; i < n; i++)
+            //{
+            //    if (number % i == 0)
+            //    {
+            //        sum = sum + i;
+            //    }
+            //}
+            //Console.WriteLine(sum);
+            ////Xndir 152
+            //int number = 70;
+            //int product = 1;
+            //for (int i = 1; i < n; i++)
+            //{
+            //    if (number % i == 0)
+            //    {
+            //        product = product * i;
+            //    }
+            //}
+            //Console.WriteLine(product);
+            ////Xndir 153
+            //int n = 10;
+            //int count = 0;
+            //for (int i = 1; i < n; i++)
+            //{
+            //    if(n % i == 2)
+            //    {
+            //        count = count + i;
+            //        //Console.WriteLine(count);
+            //    }
+            //    //Console.WriteLine(count);
+            //}
+            //Console.WriteLine(count);
+            ////Xndir 154
+            //int n = 15;
+            //int product = 1;
+            //for (int i = 1; i < n; i++)
+            //{
+            //    if (n % i == 3)
+            //    {
+            //        product = product * i;
+            //        //Console.WriteLine(i);
+            //        //Console.WriteLine(product);
+            //    }
+            //    //Console.WriteLine(product);
+            //}
+            //Console.WriteLine(product);
+            ////Xndir 155
+            ///
+            //int sum = 0;
+            //for(int i = 10; i <= 99; i++ )
+            //{
+            //    if(i % 3 == 0)
+            //    {
+            //        sum += i;
+            //    }
+            //}
+            //Console.WriteLine(sum);
+            //Xndir 156
+            //int sum = 0;
+            //for (int i = 10; i < 100; i++)
+            //{
+            //    if ( i % 3 == 0 && i % 5 ==0)
+            //    {
+            //        sum = sum + i;
+            //        //Console.WriteLine(i); // if - i paymanin bavararox arjeqner@
+            //    }
+            //    /*Console.WriteLine(i);*/ // Loop-i qayler@
+            //}
+            //Console.WriteLine(sum);
+            ////Xndir 157
+            //int sum = 0;
+            //for (int i = 100; i < 999; i++)
+            //{
+            //    if (i % 5 != 0)
+            //    {
+            //        sum = sum + i;
+            //        //Console.WriteLine(i);
+            //    }
+            //    //Console.WriteLine(i);
+            //}
+            //Console.WriteLine(sum);
+            ////Xndir 158
+            //int product = 1;
+            //for (int i = 100; i < 999; i++)
+            //{
+            //    if (i % 2 != 0 && i % 3 != 0)
+            //    {
+            //        product += i;
+            //        //Console.WriteLine(i);
+            //    }
+            //    //Console.WriteLine(i);
+            //}
+            //Console.WriteLine(product);
+            ////Xndir 159
+            //int product = 1;
+            //for (int i = 100; i < 999; i++)
+            //{
+            //    if (i % 3 == 1 && i % 4 ==2 )
+            //    {
+            //        product += i;
+            //        //Console.WriteLine(i);
+            //    }
+            //}
+            //Console.WriteLine(product);
+            ////Xndir 160
+            //for (int i = 100; i < 999; i++)
+            //{
+            //    double check = i * 16;
+            //    Math.Sqrt(check);
+            //    double result = Math.Sqrt(check);
+            //    if (result % 1 == 0)
+            //    {
+            //        Console.WriteLine(result);// vor tvi qarakusin e =
+            //        Console.WriteLine(i);// amenapoqr eranish tiv@
+            //        break;
+            //    }
+            //}
+            ////Xndir 161
+            //for (int i = 1000; i < 9999; i++)
+            //{
+            //    double check = i * 26;
+            //    Math.Sqrt(check);
+            //    double result = Math.Sqrt(check);
+            //    if (result % 1 == 0)
+            //    {
+            //        Console.WriteLine("Ays tvi qarakusin " + result + " = ");
+            //        Console.WriteLine(" 26 bazmapatkac " + i);
+            //        break;
+            //    }
+            //}
+            //Xndir 162
+            //int n = int.Parse(Console.ReadLine());
+            //for (int i = 9999; i > 99; i--)
+            //{
+            //    double check = i * 14;
+            //    Math.Sqrt(check);
+            //    double result = Math.Sqrt(check);
+            //    //Console.WriteLine(i);
+            //    if (result % 1 == 0)
+            //    {
+            //        Console.WriteLine("Ays tvi qarakusin " + result + " = ");
+            //        Console.WriteLine(" 14 bazmapatkac amenemac qaranish tive@ vorn e " + i);
+            //        break;
+            //    }
+            //}
+            ////Xndir 163
+            //for (int i = 9999; i > 99; i--)
+            //{
+            //    double check = i * 18;
+            //    Math.Sqrt(check);
+            //    double result = Math.Sqrt(check);
+            //    if (result % 1 == 0)
+            //    {
+            //        Console.WriteLine("Ays tvi qarakusin " + result + " = ");
+            //        Console.WriteLine(" 18 bazmapatkac amenemac qaranish tive@ vorn e " + i);
+            //        break;
+            //    }
+            //}
+            ////Xndir 164
+            ////int n = 1;
+            //Console.WriteLine("Mutqagreq N bnakan tiv ");
+            //int n = int.Parse(Console.ReadLine());
+            //for (int i = 100; i < 1000; i++)
+            //{
+            //    if (i  > n * n)
+            //    {
+            //        Console.WriteLine("Amenapoqr eranish tivn e, vori qarakusi armat@ mec mutqagrvac N bnakan tvic");
+            //        Console.WriteLine(i);
+            //        break;
+            //    }
+            //}
+            //Xndir 165
+
+            ////Boolean t = true;
+            //Console.WriteLine("Mutqagreq N bnakan tiv ");
+            //int n = int.Parse(Console.ReadLine());
+            //bool t = true;
+            //for (int i = n; i > 0; i = i / 3)
+            //{
+            //    if (i % 3 != 0 && i != 1)
+            //    {
+            //        t = false;
+            //        break;
+            //    }
+            //}
+            //Console.WriteLine(t);
+
+            /*if (n % 3 == 0)
+            {
+                t = true;
+                Console.WriteLine("Ayd tivn e");
+                Console.WriteLine(n);
+            }
+            else
+            {
+                t = false;
+                Console.WriteLine("Tive 3 astichan che");
+            }*/
+            // Xndir 166
+
+           /* int n = int.Parse(Console.ReadLine());
+            int y = 1;
+            for (int i = 0; i>0; i++)
+            {
+                if (i%4==0)
+                    y = 0;
+                    break;
+                }
+            }
+
+
+
+*/
+
+
+
+        }
+    }
+}
